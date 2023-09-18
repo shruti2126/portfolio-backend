8edc4ab09d9e1e936e3a52f61d98196f0d856e76
Shruti <ss.sharma1826@gmail.com>
2023-09-17 22:41:34 -0500

    debugging

--- index.js
+++ index.js
@@ -42,9 +42,4 @@ app.post("/send-email", (req, res) => {
   });
 });
 
-// // Start the server
-// app.listen(8080, () => {
-//   console.log("Server is running on port 8080");
-// });
-
 module.exports = app;
