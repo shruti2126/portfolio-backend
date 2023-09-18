9672ca81aee3f3ee7817a3027dd21d10b7993c7a
Shruti <ss.sharma1826@gmail.com>
2023-09-18 01:03:51 -0500

    trying to deploy on render

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const nodemailer = require("nodemailer");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
