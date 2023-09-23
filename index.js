e4173054775992a0509b2af7d9aaebeee64d6f50
Shruti <ss.sharma1826@gmail.com>
2023-09-22 22:00:44 -0500

    Cors origin error

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://shrutis.io"],
+    origins: ["http://localhost:3000", "https://shrutis.io"],
     methods: "GET, POST, PUT, DELETE",
     allowedHeaders: "Content-Type",
     credentials: true,
